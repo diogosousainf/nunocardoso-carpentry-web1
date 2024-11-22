7090ffcb2ba9195cfbf3ea84dfcdf46561877616
Diogo Sousa <diogosousainf@gmail.com>
2024-11-22 22:25:46 +0000

    portas

--- src/app/pages/doors/doors.component.ts
+++ src/app/pages/doors/doors.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgClass, NgForOf} from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-doors',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgClass
+  ],
   templateUrl: './doors.component.html',
-  styleUrl: './doors.component.css'
+  styleUrls: ['./doors.component.css']
 })
-export class DoorsComponent {
+export class DoorsComponent implements OnInit {
+  images: string[] = [
+    'p1.webp', 'p2.webp', 'p3.webp', 'p10.webp', 'p11.webp', 'p12.webp', 'p14.webp', 'p15.webp', 'p16.webp', 'p19.webp',
+    'p21.webp', 'p22.webp', 'p25.webp', 'p26.webp', 'p27.webp', 'p28.webp', 'p29.webp', 'p30.webp', 'p31.webp', 'p32.webp',
+    'porta-menu.JPEG', 'pr1.jpg', 'pr2.jpg', 'pr3.jpg'
+  ];
 
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    // Código para inicialização se necessário
+  }
+
+  getSanitizedUrl(url: string) {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
 }
