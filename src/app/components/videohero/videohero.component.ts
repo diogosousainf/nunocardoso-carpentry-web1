a9e69d8be621df8d922cbad2b094aae7fba22326
Diogo Sousa <diogosousainf@gmail.com>
2024-11-24 10:56:29 +0000

    video hero

--- src/app/components/videohero/videohero.component.ts
+++ src/app/components/videohero/videohero.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import Plyr from 'plyr';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-videohero',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './videohero.component.html',
   styleUrl: './videohero.component.css'
 })
-export class VideoheroComponent {
+export class VideoheroComponent  implements AfterViewInit {
 
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      import('plyr').then(Plyr => {
+        const videoElement = document.querySelector('#my-video') as HTMLVideoElement;
+
+        if (videoElement) {
+          new Plyr.default(videoElement, {
+            controls: [], // Remover todos os controles, deixando o vídeo limpo
+            autoplay: true,
+            muted: true,
+            loop: {
+              active: true,
+            }
+          });
+        }
+      }).catch(error => console.error('Erro ao carregar o Plyr:', error));
+    }
+  }
 }
