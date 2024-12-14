7e54b433543afd0edbb5890b1ebe35a4df48adb9
Diogo Sousa <diogosousainf@gmail.com>
2024-12-14 15:56:20 +0000

    1

--- src/app/pages/wardrobes/wardrobes.component.ts
+++ src/app/pages/wardrobes/wardrobes.component.ts
@@ -15,10 +15,10 @@ import {NgForOf, NgClass, NgIf} from '@angular/common';
 })
 export class WardrobesComponent implements OnInit {
   images: string[] = [
-     'IMG_1093.JPG', 'IMG_1095.JPG', 'IMG_1097.JPG', 'IMG_1098.JPG', 'IMG_1129.JPG', 'IMG_1130.JPG', 'IMG_1139.JPG', 'IMG_1140.JPG',
-     'IMG_1141.JPG', 'IMG_1142.JPG', 'IMG_1152.JPG', 'IMG_1154.JPG', 'IMG_1155.JPG', 'IMG_1156.JPG', 'IMG_1158.JPG', 'IMG_1160.JPG',
-     'IMG_1161.JPG', 'IMG_1164.JPG', 'IMG_1165.JPG', 'IMG_1166.JPG', 'IMG_1167.JPG', 'IMG_1169.JPG', 'IMG_1170.JPG', 'IMG_1171.JPG',
-     'IMG_1172.JPG', 'IMG_1174.JPG', 'IMG_1175.JPG', 'IMG_1176.JPG',
+     'IMG_1093.JPG', 'IMG_1095.JPG','IMG_1098.JPG', 'IMG_1129.JPG',  'IMG_1140.JPG',
+      'IMG_1142.JPG', 'IMG_1152.JPG', 'IMG_1154.JPG', 'IMG_1155.JPG', 'IMG_1156.JPG', 'IMG_1158.JPG', 'IMG_1160.JPG',
+     'IMG_1161.JPG', 'IMG_1164.JPG', 'IMG_1165.JPG', 'IMG_1166.JPG', 'IMG_1167.JPG', 'IMG_1169.JPG', 'IMG_1170.JPG', 
+     'IMG_1172.JPG',  'IMG_1175.JPG', 'IMG_1176.JPG',
      'a2.webp',  'a4.webp','a6.webp', 'a7.webp', 'a8.webp',
       'a11.webp', 'a12.webp',  'a14.webp', 'a15.webp', 'a16.webp',
     'a18.webp', 'a19.webp', 'a21.webp', 'a22.webp', 'a23.jpg', 'a24.jpg', 'a25.jpg', 'a26.jpg'
