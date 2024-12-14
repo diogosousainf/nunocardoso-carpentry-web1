ad70a7a54cd05b8c8822ca70b6a297d1234d6c37
Diogo Sousa <diogosousainf@gmail.com>
2024-12-14 19:55:20 +0000

    12

--- src/app/components/hero-carousel/hero-carousel.component.ts
+++ src/app/components/hero-carousel/hero-carousel.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero-carousel',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './hero-carousel.component.html',
   styleUrl: './hero-carousel.component.css'
 })
 export class HeroCarouselComponent {
-
 }
