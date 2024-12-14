8e53c318e0898543f533a2ae3aaa610d57159f8f
Diogo Sousa <diogosousainf@gmail.com>
2024-12-14 15:01:32 +0000

    new images

--- src/app/pages/doors/doors.component.ts
+++ src/app/pages/doors/doors.component.ts
@@ -15,9 +15,9 @@ import GLightbox from 'glightbox';
 })
 export class DoorsComponent  implements OnInit {
   images: string[] = [
-    'pr1.jpg', 'pr2.jpg', 'pr3.jpg','p1.webp', 'p2.webp', 'p3.webp', 'p11.webp', 'p12.webp', 'p15.webp', 'p16.webp', 'p19.webp',
-    'p21.webp',  'p25.webp', 'p26.webp', 'p27.webp','p29.webp', 'p30.webp', 'p31.webp', 'p32.webp',
-    'porta-menu.JPEG'
+    'IMG_1131.JPG', 'IMG_1133.JPG', 'pr2.jpg', 'pr3.jpg', 'p2.webp', 'p3.webp', 'p11.webp', 'p15.webp',  'p19.webp',
+    'p21.webp', 'p27.webp','p29.webp', 'p30.webp', 'p31.webp', 'p32.webp', 
+    'porta-menu.JPEG' 
   ];
 
   constructor(private sanitizer: DomSanitizer) {}
--- src/app/pages/general-carpentry/general-carpentry.component.ts
+++ src/app/pages/general-carpentry/general-carpentry.component.ts
@@ -13,9 +13,11 @@ import {NgForOf} from '@angular/common';
 })
 export class GeneralCarpentryComponent implements OnInit {
   images: string[] = [
-    '1.webp', '2.webp', '3.webp', '4.webp', '5.webp', '6.webp', '7.webp', '8.webp',
-    '9.webp', '10.webp', '11.webp', '12.webp', '13.webp', '14.webp', '15.webp', '16.webp', '18.webp',  '20.webp', '22.webp', '23.webp', '24.webp',
-    '25.webp', '26.webp', '27.webp', '28.webp', '30.webp', '31.webp'
+    'IMG_1120.JPG' , 'IMG_1122.JPG' , 'IMG_1123.JPG' , 'IMG_1124.JPG' , 'IMG_1125.JPG' , 'IMG_1126.JPG' , 'IMG_1127.JPG','IMG_1128.JPG',
+    'IMG_1136.JPG','IMG_1137.JPG', 'IMG_1138.JPG','IMG_1144.JPG','IMG_1145.JPG','IMG_1178.JPG','cgb3.jpg','cgb2.jpg',
+    '1.webp', '2.webp', '4.webp', '5.webp', '6.webp', '7.webp', '8.webp',
+    '9.webp', '10.webp', '11.webp', '12.webp',   '18.webp', 
+    '25.webp', '27.webp', '28.webp', '30.webp', '31.webp'
   ];
 
   constructor(private sanitizer: DomSanitizer) {}
