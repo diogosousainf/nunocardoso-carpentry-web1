5b43763a43f95e37729128abc8fbc7ed3dec147c
Diogo Sousa <diogosousainf@gmail.com>
2024-12-16 20:31:01 +0000

    1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import {CookieService} from '../cookie.service';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
 }
 
 export const appConfig: ApplicationConfig = {
