70d359d872da96c1a4577532158d9b0c9a0b3737
Diogo Sousa <diogosousainf@gmail.com>
2024-12-17 21:12:17 +0000

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {CookieBannerComponent} from './components/cookie-banner/cookie-banner.component';
+import { NgOptimizedImage } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent,CookieBannerComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent,CookieBannerComponent ,NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
