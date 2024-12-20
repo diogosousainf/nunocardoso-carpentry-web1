c48e767bb36f22251ea4956b9b360c9e5dd1da56
Diogo Sousa <diogosousainf@gmail.com>
2024-12-20 11:48:35 +0000

    1

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-footer',
   standalone: true,
