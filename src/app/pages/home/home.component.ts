7c0d6e11e8f4aa5ae68cb7b4cceb3b25bf3f9e3b
Diogo Sousa <diogosousainf@gmail.com>
2024-12-22 23:49:25 +0000

    some changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,7 +4,6 @@ import {ProductsSectionComponent} from '../../components/products-section/produc
 import {AboutUsComponent} from '../../components/about-us/about-us.component';
 import {CompanyComponent} from '../../components/company/company.component';
 import {TalktousComponent} from '../../components/talktous/talktous.component';
-import { ProductNewSectionComponent } from '../../components/product-new-section/product-new-section.component';
 import { NgIf} from '@angular/common';
 
 
@@ -17,25 +16,11 @@ import { NgIf} from '@angular/common';
     AboutUsComponent,
     CompanyComponent,
     TalktousComponent,
-    ProductNewSectionComponent, 
     NgIf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  isDesktop: boolean = false;
 
-  ngOnInit(): void {
-    this.checkScreenWidth(); // Verificar largura ao carregar
-  }
-
-  @HostListener('window:resize', [])
-  onResize(): void {
-    this.checkScreenWidth(); // Atualizar quando a janela for redimensionada
-  }
-
-  private checkScreenWidth(): void {
-    this.isDesktop = window.innerWidth >= 1024; // Detectar telas maiores ou iguais a 1024px
-  }
 }
