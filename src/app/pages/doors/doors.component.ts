320b2a05d8e689f99978c8b7ecafb32279f04623
Diogo Sousa <diogosousainf@gmail.com>
2025-02-03 11:00:53 +0000

    adiçao de obra de aveleda

--- src/app/pages/doors/doors.component.ts
+++ src/app/pages/doors/doors.component.ts
@@ -19,7 +19,7 @@ import { Meta, Title } from '@angular/platform-browser';
 export class DoorsComponent implements OnInit {
 
   images: string[] = [
-    'IMG_1131.JPG', 'IMG_1133.JPG', 'pr2.jpg', 'pr3.jpg', 'p2.webp', 'p3.webp',
+    'p1-ac.webp','p2-ac.webp','IMG_1131.JPG', 'IMG_1133.JPG', 'pr2.jpg', 'pr3.jpg', 'p2.webp', 'p3.webp',
     'p11.webp', 'p15.webp', 'p19.webp', 'p21.webp', 'p27.webp','p29.webp',
     'p30.webp', 'p31.webp', 'p32.webp'
   ];
